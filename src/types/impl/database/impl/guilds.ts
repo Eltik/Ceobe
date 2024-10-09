3975847a7cc26441ed1e0f5f142d8a7eaec24248
Eltik <xp10d363@gmail.com>
2024-10-09 16:29:39 -0700

    feat: Getting guilds + types

--- src/types/impl/database/impl/guilds.ts
+++ src/types/impl/database/impl/guilds.ts
@@ -1,5 +1,46 @@
 import type { SchemaToInput, SchemaToUpdateInput } from "..";
 import { table } from "../../../../database/impl/tables/guilds";
+import { User } from "./users";
 
 export type CreateGuildInput = SchemaToInput<typeof table>;
 export type UpdateGuildInput = SchemaToUpdateInput<typeof table> & { id: string };
+export type GetGuildInput = { id: string };
+
+export type Guild = {
+    id: string;
+    guild_id: string;
+    channels: Channel[];
+    users: User[];
+    roles: Role[];
+    created_at: Date;
+};
+
+export type Channel = {
+    id: string;
+    // Metadata from Discord API
+    metadata: {
+        name: string;
+        description?: string;
+        position?: number;
+        rateLimitPerUser?: number;
+        nsfw?: boolean;
+        bitrate?: number;
+        userLimit?: number;
+        permissionOverwrites?: Record<string, any>[];
+        parentID?: string;
+    };
+    type: string;
+};
+
+export type Role = {
+    id: string;
+    // Metadata from Discord API
+    metadata: {
+        name: string;
+        permissions: string[];
+        color: number;
+        hoist: boolean;
+        mentionable: boolean;
+    };
+    type: string;
+};
--- src/types/impl/database/impl/users.ts
+++ src/types/impl/database/impl/users.ts
@@ -3,3 +3,13 @@ import { table } from "../../../../database/impl/tables/users";
 
 export type CreateUserInput = SchemaToInput<typeof table>;
 export type UpdateUserInput = SchemaToUpdateInput<typeof table> & { id: string };
+export type GetUserInput = { id: string };
+
+export type User = {
+    id: string;
+    guild_id: string;
+    user_id: string;
+    points: number;
+    level: number;
+    created_at: Date;
+};
